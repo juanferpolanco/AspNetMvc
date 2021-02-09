da361b94621eb2dc613256d3a71ade345153dfe9
unknown <juan.polanco.franco@gmail.com>
2021-02-09 14:17:36 -0500

    AspNetVideo

---
+++ VideoMVC/Controllers/ContratosController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VideoMVC.Models;
+
+namespace VideoMVC.Controllers
+{
+    public class ContratosController : Controller
+    {
+        private dbVideoMVCEntities db = new dbVideoMVCEntities();
+
+        // GET: Contratos
+        public ActionResult Index()
+        {
+            var tblContrato = db.tblContrato.Include(t => t.tblCliente);
+            return View(tblContrato.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(DateTime From, DateTime To)
+        {
+            var array = db.BuscarDato(From, To);
+            return View(array);
+        }
+
+        // GET: Contratos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblContrato tblContrato = db.tblContrato.Find(id);
+            if (tblContrato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblContrato);
+        }
+
+        // GET: Contratos/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCliente = new SelectList(db.tblCliente, "idCliente", "nombreCliente");
+            return View();
+        }
+
+        // POST: Contratos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idContrato,idCliente,montoContrato,fechaContrato")] tblContrato tblContrato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblContrato.Add(tblContrato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCliente = new SelectList(db.tblCliente, "idCliente", "nombreCliente", tblContrato.idCliente);
+            return View(tblContrato);
+        }
+
+        // GET: Contratos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblContrato tblContrato = db.tblContrato.Find(id);
+            if (tblContrato == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCliente = new SelectList(db.tblCliente, "idCliente", "nombreCliente", tblContrato.idCliente);
+            return View(tblContrato);
+        }
+
+        // POST: Contratos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idContrato,idCliente,montoContrato,fechaContrato")] tblContrato tblContrato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblContrato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCliente = new SelectList(db.tblCliente, "idCliente", "nombreCliente", tblContrato.idCliente);
+            return View(tblContrato);
+        }
+
+        // GET: Contratos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblContrato tblContrato = db.tblContrato.Find(id);
+            if (tblContrato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblContrato);
+        }
+
+        // POST: Contratos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblContrato tblContrato = db.tblContrato.Find(id);
+            db.tblContrato.Remove(tblContrato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VideoMVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VideoMVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VideoMVC")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1073d0b9-0666-4a52-b596-c42f41c3ff34")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
